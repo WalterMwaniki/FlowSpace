5a037b1b4fee0972536a93d5d52f5ddd916d3e40
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2023-02-05 04:28:21 -0500

    page navigation done

--- client/app/SidebarItem.tsx
+++ client/app/SidebarItem.tsx
@@ -1,11 +1,26 @@
-import styles from '../styles/sidebar.module.scss';
+'use client'
 
+import styles from '../styles/sidebar.module.scss';
+import { useRouter } from 'next/navigation';
 
 interface sidebarItem_props { label: string, icon: React.ReactNode };
 
-const SidebarItem = ({label, icon}: sidebarItem_props): JSX.Element => {
+const SidebarItem = ({ label, icon }: sidebarItem_props): JSX.Element => {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (label === 'Home') {
+      router.push('/');
+    }
+    else {
+      router.push(`/${label.toLowerCase()}`);
+    }
+
+  };
+
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={handleClick} >
       {icon}
       <p>{label}</p>
     </div>
---
+++ client/app/stats/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <a>Stats</a>
+    </div>
+  );
+}
