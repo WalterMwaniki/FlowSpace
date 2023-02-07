198094b3af24026731831ee2d22ba6ab8b26b35c
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2023-02-07 03:26:09 -0500

    inital page navigation complete

--- client/app/SidebarItem.tsx
+++ client/app/SidebarItem.tsx
@@ -9,14 +9,20 @@ const SidebarItem = ({ label, icon }: sidebarItem_props): JSX.Element => {
 
   const router = useRouter();
 
-  const handleClick = () => {
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (label === 'Home') {
       router.push('/');
     }
     else {
       router.push(`/${label.toLowerCase()}`);
     }
-
+    
+    const items = document.getElementsByClassName(styles.item);
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove(styles.active);
+    }
+    e.currentTarget.classList.add(styles.active);
+    
   };
 
   return (
