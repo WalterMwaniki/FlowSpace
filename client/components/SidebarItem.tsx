e17bb74425a19c0045bf8f88992b091d641e16c8
Walter Mwaniki <mwaniki.faraja@gmail.com>
2023-02-07 20:05:08 -0500

    fixed routing mistake created by merging

--- client/components/SidebarItem.tsx
+++ client/components/SidebarItem.tsx
@@ -1,31 +1,51 @@
+// import styles from "../styles/sidebar.module.scss";
+
+// interface sidebarItem_props {
+//   label: string;
+//   isSelected: boolean;
+//   icon: React.ReactNode;
+// }
+
+// const SidebarItem = ({
+//   label,
+//   isSelected,
+//   icon,
+// }: sidebarItem_props): JSX.Element => {
+//   if (isSelected) {
+//     return (
+"use client";
+
 import styles from "../styles/sidebar.module.scss";
+import { useRouter } from "next/navigation";
 
 interface sidebarItem_props {
   label: string;
-  isSelected: boolean;
   icon: React.ReactNode;
 }
 
-const SidebarItem = ({
-  label,
-  isSelected,
-  icon,
-}: sidebarItem_props): JSX.Element => {
-  if (isSelected) {
-    return (
-      <div className={`${styles.item} ${styles.active}`}>
-        {icon}
-        <p>{label}</p>
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.item}>
-        {icon}
-        <p>{label}</p>
-      </div>
-    );
-  }
+const SidebarItem = ({ label, icon }: sidebarItem_props): JSX.Element => {
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (label === "Home") {
+      router.push("/");
+    } else {
+      router.push(`/${label.toLowerCase()}`);
+    }
+
+    const items = document.getElementsByClassName(styles.item);
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove(styles.active);
+    }
+    e.currentTarget.classList.add(styles.active);
+  };
+
+  return (
+    <div className={styles.item} onClick={handleClick}>
+      {icon}
+      <p>{label}</p>
+    </div>
+  );
 };
 
 export default SidebarItem;
